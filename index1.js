22c36c80e40331d3d6a0a0317affdf42d2963530
Arpit-416 <72140728+Arpit-416@users.noreply.github.com>
2020-09-30 18:40:46 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,23 @@
+let interestRate =1;
+console.log(interestRate);
+let number= 1;
+let defer= undefined;
+let boolean =false;
+let name ='Arpit';
+let firstname=null;
+
+let person = {
+
+    name:'Josh',
+    age:24
+
+};
+person.name='john';
+console.log(person.name);
+person['age']=27;
+console.log(person.age);
+
+let selectedColors = ["red","blue"]
+console.log(selectedColors);
+
+//console.log(person.age);
